7ffd7707f3adf74a9da0eb625aadf757c664ce95
Rick Smith <ricochet2200@gmail.com>
2015-06-16 15:15:57 -0600

    Add more examples to rect-main

--- examples/rect/rect-main.go
+++ examples/rect/rect-main.go
@@ -10,16 +10,25 @@ import (
 )
 
 func main() {
-	src := image.NewRGBA(image.Rect(0, 0, 2, 10))
+	src := image.NewRGBA(image.Rect(0, 0, 10, 10))
 	drawer.Set(src, color.RGBA{0, 0, 255, 255})
 
-	rect := image.Rect(1, 1, 1, 9)
-
+	rect := image.Rect(0, 0, 10, 10)
 	rd := drawer.NewRectDrawer(src, rect, color.RGBA{0, 255, 0, 255})
-	rd.SetFilled(false)
-	rd.SetThickness(20).Draw()
 
-	filename := "./output.png"
+	draw(rd, src, "full-square.png")
+	rd.SetThickness(3).Draw()
+	draw(rd, src, "three-pixel-border.png")
+	rd.SetThickness(30).Draw()
+	draw(rd, src, "oversized-border.png")
+	rd.SetFilled(true)
+	draw(rd, src, "filled.png")
+	rd.SetColor(color.RGBA{0, 0, 0, 255})
+	draw(rd, src, "black.png")
+
+}
+
+func draw(drawer *drawer.RectDrawer, src image.Image, filename string) {
 	out, err := os.Create(filename)
 	if err != nil {
 		fmt.Println(err)
