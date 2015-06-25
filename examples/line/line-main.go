06f2522bd7dc36280ebc4f24088036bbc4b78ac7
Rick Smith <rsmith@techcyte.com>
2015-06-25 17:00:51 -0600

    Add another example of a line that is thicker

--- examples/line/line-main.go
+++ examples/line/line-main.go
@@ -13,15 +13,14 @@ func main() {
 	src := image.NewRGBA(image.Rect(0, 0, 100, 100))
 	drawer.Fill(src, color.RGBA{0, 255, 255, 255})
 
-	start := image.Pt(100, 100)
-	end := image.Pt(0, 0)
-	ld := drawer.NewLineDrawer(src, start, end, color.RGBA{255, 0, 0, 255}).Draw()
+	ld := drawer.NewLineDrawer(src, image.Pt(100, 100), image.Pt(0, 0), color.RGBA{255, 0, 0, 255}).Draw()
 	draw(ld, src, "negative.png")
 
-	start = image.Pt(0, 100)
-	end = image.Pt(100, 0)
-	ld.SetStart(end).SetEnd(start).Draw()
+	ld.SetStart(image.Pt(0, 100)).SetEnd(image.Pt(100, 0)).Draw()
 	draw(ld, src, "positive.png")
+
+	ld.SetStart(image.Pt(0, 50)).SetEnd(image.Pt(100, 50)).SetThickness(5).Draw()
+	draw(ld, src, "thick.png")
 }
 
 func draw(drawer *drawer.LineDrawer, src image.Image, filename string) {
