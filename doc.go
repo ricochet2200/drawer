3ada697eb00ad153de8d1483dc702aceb071b36f
Rick Smith <rsmith@techcyte.com>
2015-06-25 16:43:51 -0600

    Add indention in docs

--- doc.go
+++ doc.go
@@ -3,16 +3,16 @@ This package contains several structs for drawing, all following a similar conve
 
 Structs have a constructor function that takes basic arguments for that type, ie:
 
-func NewLineDrawer(draw.Image, image.Point, image.Point, color.Color)
+    func NewLineDrawer(draw.Image, image.Point, image.Point, color.Color)
 
 All functions in the Drawer structs return a pointer to itself to allow chaining:
 
-NewLineDrawer(src, start, end, color.RGBA{255, 0, 0, 255}).SetThickness(10).Draw()
+    NewLineDrawer(src, start, end, color.RGBA{255, 0, 0, 255}).SetThickness(10).Draw()
 
 This also allows you to reuse an instance:
 
-lineDrawer := NewLineDrawer(draw.Image, image.Point, image.Point, color.Color).Draw()
-lineDrawer = lineDrawer.SetStart(image.Pt(0, 50)).Draw()
+    lineDrawer := NewLineDrawer(draw.Image, image.Point, image.Point, color.Color).Draw()
+    lineDrawer = lineDrawer.SetStart(image.Pt(0, 50)).Draw()
 
 This construct allows easy addition of new features without compromising reverse compatibilty.
 */
