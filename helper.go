4cd0360d0bae409156ed02ed8e8972ec89bf86a9
Rick Smith <rsmith@techcyte.com>
2015-06-25 16:55:24 -0600

    Update docs on helper functions

--- helper.go
+++ helper.go
@@ -12,19 +12,22 @@ func Fill(img draw.Image, color color.Color) {
 	draw.Draw(img, img.Bounds(), &image.Uniform{color}, image.ZP, draw.Src)
 }
 
+// A convineice function that creates a color.RGBA struct from color.Color interface.  While fairly
+// straitforward, tedious conversions need to be made to change between uint32 and uint8.
 func ColorToRGBA(c color.Color) color.RGBA {
 	r, g, b, a := c.RGBA()
 	return color.RGBA{uint8(r / 0x101), uint8(g / 0x101), uint8(b / 0x101), uint8(a / 0x101)}
 }
 
+// Performs alpha blending on the pixel on img.At(x,y).  While draw.Set(x,y) *sets* the color to c,
+// Blend uses the existing background to create a new color that is a mix of the existing annew color.
+// This is similar to treating the two colors as stained glass windows laying over eachother.  Not that if
+// the new color is completely opache, Blend() and Set() have the same result.
 func Blend(img draw.Image, x int, y int, c color.Color) {
 	dst := ColorToRGBA(img.At(x, y))
 	src := ColorToRGBA(c)
 
 	fmt.Println("dst:", dst, "src:", src)
-
-	//	dst: {0 255 255 255} src: {255 0 0 74}
-	//	col: {1 0 0 147}
 
 	alpha := src.A + dst.A*(1-src.A)
 	r := (src.R + dst.R*(1-src.A))
